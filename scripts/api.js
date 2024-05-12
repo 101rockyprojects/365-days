dbd299c00a9b4ea5d5b4ee9b516edc226e61f024
101rockyprojects <rockybarrios10@gmail.com>
2024-05-12 16:26:16 -0500

    Feat song card, random quote and title change animation

--- scripts/api.js
+++ scripts/api.js
@@ -1,37 +1,96 @@
 import { PLAYLIST_ID, API_KEY } from './globals.js';
 
-const today = new Date();
-const start = new Date(today.getFullYear(), 0, 0);
-const diff = today - start;
-const oneDay = 1000 * 60 * 60 * 24;
-const dayIndex = Math.floor(diff / oneDay);
-
-const pageToken = dayIndex % 50; // Cada página tiene 50 videos
-
-const apiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=1&playlistId=${PLAYLIST_ID}&key=${API_KEY}&pageToken=${pageToken}`;
-console.log(apiUrl);
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    if (data.items.length > 0) {
-      const title = data.items[0].snippet.title;
-      const videoId = data.items[0].snippet.resourceId.videoId;
-      const videoUrl = `https://www.youtube.com/watch?v=${videoId}`;
-      showVideo({ title, videoUrl });
-    } else {
-      console.log('No se encontró ningún video para este día del año.');
+async function getTodayVideo(playlistId, apiKey) {
+    const today = new Date();
+    const dayNumber = getDayNumber(today);
+    const pageNumber = Math.ceil(dayNumber / 50);
+    let actualPage = 1;
+    let nextPageToken = '';
+    while (actualPage < pageNumber) {
+        const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}&maxResults=50&pageToken=${nextPageToken}`);
+        const data = await response.json();
+        nextPageToken = data.nextPageToken;
+        actualPage++;
     }
-  })
-  .catch(error => console.error('Error al obtener el video:', error));
+    const videoIndex = (dayNumber - 1) % 50; // Video index
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${playlistId}&key=${apiKey}&maxResults=50&pageToken=${nextPageToken}`);
+    const data = await response.json();
+    const video = data.items[videoIndex];
+
+    return video;
+}
+
+async function getVideoById(videoId, apiKey) {
+    const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`);
+    const data = await response.json();
+    return data.items[0];
+}
+
+function getDayNumber(date) {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 0);
+    const diff = date - firstDayOfYear;
+    const dayToMiliseconds = 1000 * 60 * 60 * 24;
+    const DayNumber = Math.floor(diff / dayToMiliseconds);
+    return DayNumber;
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[[\]]/g, '\\$&');
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+async function initialize() {
+    const songParam = getParameterByName('song');
+    let video;
+    if (songParam) {
+        try {
+            video = await getVideoById(songParam, API_KEY);
+        } catch (error) {
+            console.error("Error al obtener el video por ID:", error);
+        }
+    }
+    if (!video) {
+        try {
+            video = await getTodayVideo(PLAYLIST_ID, API_KEY);
+        } catch (error) {
+            console.error("Error al obtener el video del día:", error);
+        }
+    }
+    showVideo(video);
+}
 
 function showVideo(video) {
-  console.log('Video:', video);
-  const container = document.querySelector('.video-container');
-  const videoTitle = document.createElement('h3');
-  const videoElement = document.createElement('iframe');
-  videoTitle.classList.add('title');
-  videoTitle.innerText = video.title;
-  videoElement.src = video.videoUrl;
-  container.appendChild(videoElement);
+    const videoId = video.id ? video.id : video.snippet.resourceId.videoId;
+    const thumbnailUrl = video.snippet.thumbnails.high.url;
+    const thumbnailWidth = video.snippet.thumbnails.high.width;
+    const thumbnailHeight = video.snippet.thumbnails.high.height;
+    const container = document.querySelector('.video-container');
+    const videoTitle = document.createElement('h3');
+    const videoElement = document.querySelector('.video-element');
+    const todayDateElement = document.getElementById('today-date');
+    const listenNowLink = document.querySelector('.listen-now');
+    videoTitle.classList.add('title');
+    const maxTitleLength = 40;
+    const title = video.snippet.title.length > maxTitleLength ? 
+                  video.snippet.title.slice(0, maxTitleLength) + '...' : 
+                  video.snippet.title;
+    videoTitle.innerText = title;
+    videoElement.width = thumbnailWidth;
+    videoElement.height = thumbnailHeight;
+    videoElement.src = thumbnailUrl;
+    videoElement.alt = video.snippet.title;
+    const today = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = today.toLocaleDateString('es-ES', options);
+    todayDateElement.textContent = formattedDate;
+    listenNowLink.href = `https://youtube.com/watch?v=${videoId}`;
+    container.appendChild(videoTitle);
+    container.appendChild(videoElement);
 }
+
+initialize();
---
+++ scripts/text-change.js
@@ -0,0 +1,41 @@
+function animateText() {
+    const titleElement = document.getElementById('title-365');
+    const texts = ['días', 'canciones'];
+    let index = 0;
+
+    function updateText() {
+        const text = texts[index];
+        let newText = '';
+        let charIndex = 0;
+        const typingInterval = setInterval(() => {
+            newText += text[charIndex];
+            titleElement.textContent = newText;
+            charIndex++;
+            if (charIndex === text.length) {
+                clearInterval(typingInterval);
+                setTimeout(() => {
+                    const deletingInterval = setInterval(() => {
+                        newText = newText.slice(0, -1);
+                        titleElement.textContent = newText;
+                        if (newText === '') {
+                            clearInterval(deletingInterval);
+                            setTimeout(() => {
+                                index = (index + 1) % texts.length;
+                                updateText();
+                            }, 1000);
+                        }
+                    }, randomTypingTime() * 50 + 50);
+                }, 1000);
+            }
+        }, randomTypingTime() * 50 + 100);
+    }
+
+    updateText();
+}
+
+function randomTypingTime() {
+    var random = Math.floor(Math.random() * 2);
+    return random + 1;
+}
+
+animateText();
